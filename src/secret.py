f019440c41e773db34dc2a44efa629f3d6351119
andr3i-f <105520459+andr3i-f@users.noreply.github.com>
2023-11-23 16:56:16 -0800

    Created unfilled secret.py

---
+++ src/secret.py
@@ -0,0 +1,4 @@
+skinsWebhook = "" # webhook for skins channel
+stickerWebhook = "" # webhook for sticker & create channel
+updateWebhook = "" # webhook for an update channel (incase a crawler gets banned)
+bot_token = "" # token for bot
