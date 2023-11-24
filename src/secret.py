f8a1e1d43438785f6924936ad1c1bfd3cb903a2b
andr3i-f <andrei.florea003@gmail.com>
2023-11-23 19:50:14 -0800

    accidently uploaded secret.py to gh

--- src/secret.py
+++
@@ -1,8 +0,0 @@
-#key = "SB5q3AjM3vcIaAOv4xD-ZLHuLIsdmajp"
-skinsWebhook = "https://discord.com/api/webhooks/1170595376806502441/36TAtzbDI11rVpo2fi0BJjtSA4vqlTSPGDXGuwIA7l6Fi0an42Q4IricpZkTFD5opCKN"
-stickerWebhook = "https://discord.com/api/webhooks/1172602568996819044/QTaHohqozn_ObeW4yJk3NkcN00Kf8zPSz4GVCpz0kno2kQjZT62IX9S7OkCuJgmpjO4L"
-updateWebhook = "https://discord.com/api/webhooks/1170609656536780900/y9ikkVTX1sr8yujYlm0l3oicSpw5MGmbQNAvD4eLxwu6b-TuoUWvaNuSSbt9qv6r3_Wz"
-#applicationID = 1174149385253310474
-#public_key = "a1fbf767c96c3f2ca228aa1122be9c45f4ce30c9437fd17fb4c2c09d428f275a"
-bot_token = "MTE3NDgxNjMyMzg4MTE1NjcwOA.GkgfFK.WT16vobwNqVRlcYyIA1zVykHY_9aGfSlkwPyz0"
-#invite_link_bot = "https://discord.com/api/oauth2/authorize?client_id=1174816323881156708&permissions=2183991393344&scope=bot"
