b4e9b13c95b64f67e7fb761a848f4d7e77ba98a2
andr3i-f <105520459+andr3i-f@users.noreply.github.com>
2023-11-23 16:57:23 -0800

    Deleted src/secret.py - will override my secret.py if pulled

--- src/secret.py
+++
@@ -1,4 +0,0 @@
-skinsWebhook = "" # webhook for skins channel
-stickerWebhook = "" # webhook for sticker & create channel
-updateWebhook = "" # webhook for an update channel (incase a crawler gets banned)
-bot_token = "" # token for bot
